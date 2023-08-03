21c2520cc54d9344b34ccc48673152a69f11f61a
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-08-03 22:49:10 +0200

    [feat](banana): added getThrottle and getBrake

--- banana_class.py
+++ banana_class.py
@@ -7,12 +7,22 @@ class banana():
         self.y_min = y_min
         self.x_max = x_max
         self.y_max = y_max
-        self.avg_l = avg_l
-        self.avg_r = avg_r
+        self.avg_l = avg_l if avg_l else avg_r if avg_r else 1
+        self.avg_r = avg_r if avg_r else avg_l if avg_l else 1
         self.size = self.getSize()
 
+    def getThrottle(self, treshold, scale_factor=150):
+        if self.size <= treshold: return 0
+        throttle = min(100, ((self.size - treshold) / treshold)*scale_factor)
+        return int(max(throttle, 15))
+
+    def getBrake(self, treshold, scale_factor=150):
+        if self.size >= treshold: return 0
+        brake = min(100, ((treshold - self.size) / self.size)*scale_factor)
+        return int(max(brake, 15))
+
     def getSize(self):
-        return (self.x_max-self.x_min) * (self.y_max-self.y_min)
+        return (self.x_max-self.x_min) + (self.y_max-self.y_min)
 
     def getRectangle(self):
         return ((self.x_min, self.y_min), (self.x_max, self.y_max))
@@ -20,8 +30,8 @@ class banana():
     def getRotation(self):
         slope = (self.avg_r-self.avg_l)/(self.x_max - self.x_min)
 
-        #Should be (slope)/(pi/2), but works better with (pi/3) due to the object thickness
-        return -min(1, max(-1, arctan(slope)/(pi/3)))
+        #Should be (slope)/(pi/2), but works better with (pi/4) due to the object thickness
+        return -min(1, max(-1, arctan(slope)/(pi/4)))
 
     def getLinePoints(self):
         return ((int(self.x_min), int(self.avg_l)), (int(self.x_max), int(self.avg_r)))
